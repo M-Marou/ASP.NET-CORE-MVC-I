5a63c94f7afc00f73d07a1abc3751a0ac3ac7726
Marouane Moumni <MAR@YC.com>
2021-02-16 22:21:34 +0200

    added models

---
+++ StudentManagment/StudentManagement2/Models/StudentsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement2.Models
+{
+    public class StudentsViewModel
+    {
+        [Key]
+        public int Student_ID { get; set; }
+        public string Student_Name { get; set; }
+        public string Student_CIN { get; set; }
+        public string Student_Address { get; set; }
+    }
+}
---
+++ StudentManagment/StudentManagment/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagment.Controllers
+{
+    public class ProductController
+    {
+        public string Info()
+        {
+            return "from my Product Info";
+        }
+    }
+}
