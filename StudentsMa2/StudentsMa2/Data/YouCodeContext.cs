fe79cbbbdb2b42fd28c3879b10c93924b7e6931e
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2021-02-26 16:21:10 +0100

    codefirst

---
+++ StudentsMa2/StudentsMa2/Data/YouCodeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsMa2.Models.YouCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsMa2.Data
+{
+    public class YouCodeContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-StudentsManagement_III-1E032FFB-1303-425C-AEF0-5A24EFCB7CE8;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<ClassModel> StudentClass { get; set; }
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
---
+++ StudentsMa2/StudentsMa2/Models/YouCode/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsMa2.Models.YouCode
+{
+    public class StudentModel
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required(ErrorMessage = "Please add a student name")]
+        public string studentName { get; set; }
+        [Required(ErrorMessage = "Please add a CIN")]
+        public string studentCIN { get; set; }
+        [Required(ErrorMessage = "Please add an address")]
+        public string studentAddress { get; set; }
+
+        public string className { get; set; }
+        public ClassModel studentClass { get; set; }
+    }
+}
